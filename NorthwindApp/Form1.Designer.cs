6708f493cd1a307763aa10075732da7eb1951c91
Aydın SEÇER <aydin_secer@hotmail.com>
2022-05-12 15:28:39 +0300

    Add project files.

---
+++ NorthwindApp/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace NorthwindApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.gridCategoryList = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDeleteImage = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCategoryList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Description";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCategoryName.Location = new System.Drawing.Point(201, 38);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(324, 32);
+            this.txtCategoryName.TabIndex = 0;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtDescription.Location = new System.Drawing.Point(201, 80);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(324, 32);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSave.Location = new System.Drawing.Point(201, 176);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(104, 37);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.Location = new System.Drawing.Point(311, 176);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 37);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.Location = new System.Drawing.Point(421, 176);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 37);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.TabStop = false;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPicture.Location = new System.Drawing.Point(600, 38);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(145, 123);
+            this.pbPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture.TabIndex = 3;
+            this.pbPicture.TabStop = false;
+            this.pbPicture.DoubleClick += new System.EventHandler(this.pbPicture_DoubleClick);
+            // 
+            // gridCategoryList
+            // 
+            this.gridCategoryList.AllowUserToAddRows = false;
+            this.gridCategoryList.AllowUserToDeleteRows = false;
+            this.gridCategoryList.AllowUserToResizeColumns = false;
+            this.gridCategoryList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridCategoryList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCategoryList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.gridCategoryList.Location = new System.Drawing.Point(17, 307);
+            this.gridCategoryList.Name = "gridCategoryList";
+            this.gridCategoryList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridCategoryList.Size = new System.Drawing.Size(728, 226);
+            this.gridCategoryList.TabIndex = 5;
+            this.gridCategoryList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridCategoryList_CellDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "List";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(675, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 26);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Count";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(595, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 26);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Picture";
+            // 
+            // btnDeleteImage
+            // 
+            this.btnDeleteImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDeleteImage.ForeColor = System.Drawing.Color.Red;
+            this.btnDeleteImage.Location = new System.Drawing.Point(700, 9);
+            this.btnDeleteImage.Name = "btnDeleteImage";
+            this.btnDeleteImage.Size = new System.Drawing.Size(45, 27);
+            this.btnDeleteImage.TabIndex = 6;
+            this.btnDeleteImage.Text = "X";
+            this.btnDeleteImage.UseVisualStyleBackColor = true;
+            this.btnDeleteImage.Click += new System.EventHandler(this.btnDeleteImage_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNew.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNew.Location = new System.Drawing.Point(531, 37);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(33, 32);
+            this.btnNew.TabIndex = 7;
+            this.btnNew.Text = "+";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 545);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnDeleteImage);
+            this.Controls.Add(this.gridCategoryList);
+            this.Controls.Add(this.pbPicture);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCategoryList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.DataGridView gridCategoryList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDeleteImage;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ NorthwindApp/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace NorthwindApp
+{
+    public partial class Form1 : Form
+    {
+        public string conString { get; set; } = "Server=.;Database=NORTHWND;Trusted_Connection=True;";
+
+        SqlConnection con;
+
+        public int SelectedCategoryId { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(conString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetCategories();
+
+            btnSave.Enabled = true;
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;
+
+        }
+
+        void SaveCategory()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "insert into categories(CategoryName,Description,Picture) values (@CategoryName,@Description,@Picture)";
+
+            MemoryStream ms = new MemoryStream();
+            byte[] picture = null;
+            if (pbPicture.Image != null)
+            {
+                pbPicture.Image.Save(ms, ImageFormat.Bmp);
+                picture = ms.ToArray();
+            }
+
+            sqlCommand.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text);
+            sqlCommand.Parameters.AddWithValue("@Description", txtDescription.Text);
+
+            if (picture == null)
+            {
+                SqlParameter ip = new SqlParameter("@Picture", SqlDbType.Image);
+                ip.Value = DBNull.Value;
+                sqlCommand.Parameters.Add(ip);
+            }
+            else
+            {
+                sqlCommand.Parameters.AddWithValue("@Picture", picture);
+            }
+
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            sqlCommand.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("New category created");
+            GetCategories();
+            btnNew.PerformClick();
+        }
+
+        void UpdateCategory()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "Update categories set CategoryName=@CategoryName,Description=@Description, Picture=  @Picture Where CategoryId = @CategoryID";
+
+            MemoryStream ms = new MemoryStream();
+            byte[] picture = null;
+            if (pbPicture.Image != null)
+            {
+                pbPicture.Image.Save(ms, ImageFormat.Bmp);
+                picture = ms.ToArray();
+            }
+
+
+
+            sqlCommand.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text);
+            sqlCommand.Parameters.AddWithValue("@Description", txtDescription.Text);
+            sqlCommand.Parameters.AddWithValue("@CategoryID", SelectedCategoryId);
+
+            if (picture == null)
+            {
+                SqlParameter ip = new SqlParameter("@Picture", SqlDbType.Image);
+                ip.Value = DBNull.Value;
+                sqlCommand.Parameters.Add(ip);
+            }
+            else
+            {
+                sqlCommand.Parameters.AddWithValue("@Picture", picture);
+            }
+
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            sqlCommand.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Category Updated");
+            GetCategories();
+        }
+
+        private void DeleteCategory()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "delete from categories  Where CategoryId = @CategoryID";
+
+            sqlCommand.Parameters.AddWithValue("@CategoryID", SelectedCategoryId);
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            sqlCommand.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Category Deleted");
+            btnNew.PerformClick();
+            GetCategories();
+        }
+        void GetCategories()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "select * from Categories";
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            adapter.Fill(dt);
+
+            gridCategoryList.DataSource = dt;
+
+        }
+
+        void GetCategory()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "select * from Categories Where CategoryId = @CategoryID";
+            sqlCommand.Parameters.AddWithValue(@"CategoryID", SelectedCategoryId);
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            SqlDataReader dr = sqlCommand.ExecuteReader();
+
+            pbPicture.Image = null;
+
+            while (dr.Read())
+            {
+                txtCategoryName.Text = dr.GetString(1);
+                txtDescription.Text = dr.GetString(2);
+
+                if (dr["Picture"] != DBNull.Value)
+                {
+                    var pictureBytes = (byte[])dr["Picture"];
+
+                    var image = (Image)(new ImageConverter().ConvertFrom(pictureBytes));
+
+                    pbPicture.Image = image;
+                }
+
+            }
+
+            con.Close();
+
+        }
+
+        void DeleteImage()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = con;
+            sqlCommand.CommandText = "update categories set Picture = @Picture Where CategoryId = @CategoryID ";
+
+
+            SqlParameter ip = new SqlParameter("@Picture", SqlDbType.Image);
+            ip.Value = DBNull.Value;
+            sqlCommand.Parameters.Add(ip);
+
+            sqlCommand.Parameters.AddWithValue("@CategoryID", SelectedCategoryId);
+
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            sqlCommand.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Image Deleted");
+            pbPicture.Image = null;
+            GetCategories();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveCategory();
+        }
+
+        private void pbPicture_DoubleClick(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pbPicture.ImageLocation = fileDialog.FileName;
+            }
+        }
+
+        private void gridCategoryList_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectedData = gridCategoryList.SelectedRows[0].Cells[0].Value;
+            SelectedCategoryId = (int)selectedData;
+
+            btnSave.Enabled = false;
+            btnDelete.Enabled = true;
+            btnUpdate.Enabled = true;
+
+            GetCategory();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            SelectedCategoryId = -1;
+            txtCategoryName.Clear();
+            txtDescription.Clear();
+            pbPicture.Image = null;
+
+            btnSave.Enabled = true;
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;
+
+        }
+
+        private void btnDeleteImage_Click(object sender, EventArgs e)
+        {
+            if (pbPicture.Image != null)
+            {
+                if (MessageBox.Show("Are you sure?", "Delete Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    DeleteImage();
+                }
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateCategory();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Delete Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                DeleteCategory();
+            }
+        }
+
+
+    }
+}
